6160f55221855d04012364197affec66bca53459
majoop-0602 <majordzbllts2006@gmail.com>
2026-03-01 19:08:19 -0600

    Implement '/pagos' route to retrieve payment data
    Added a new route '/pagos' to fetch payment data from the database.

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+
+from flask import Flask, render_template, request, jsonify, make_response, session
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/pagos')
+def pagos():
+    import mysql.connector
+
+    mydb = mysql.connector.connect(
+        host="46.28.42.226",
+        user="u760464709_24005242_usr",
+        password="u7?Jpkt>Y*E7",
+        database="u760464709_24005242_bd"
+    )
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * FROM pagos")
+    myresult = mycursor.fetchall()
+    return make_response(jsonify(myresult))
